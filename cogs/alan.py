8a3086c9a0b5d747dce696f199665645e6bc84bc
Luke Thistlethwaite <lukethistle.va@gmail.com>
2021-04-15 22:31:50 -0400

    Add alanify command

--- cogs/alan.py
+++ cogs/alan.py
@@ -1,27 +1,55 @@
 import discord
 from discord.ext import commands
+from modules.modification import MediaScaler
 import PIL
+
+embed_template = discord.Embed(
+    color = int("8f0bbf", 16),
+    title = "Alanify",
+)
 
 class Alan(commands.Cog):
     """Set of commands to downscale then upscale images (inspired by Alan)"""
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group()
-    async def alan(self, ctx):
-        if ctx.invoked_subcommand is None:
-            await ctx.send("Invalid subcommand")
-
-    @alan.command()
-    async def beautify(self, ctx):
+    @commands.command()
+    async def alanify(self, ctx, value : int):
         uid = ctx.author.id
+        #make sure user is replying to a message
         replied = ctx.message.reference
         if not replied:
             return
         msg = await ctx.fetch_message(replied.message_id)
-        print(msg.embeds[0].url)
-        print(msg.)
 
+        if msg.attachments:
+            image_url = msg.attachments[0].url
+            print("passed")
+        elif msg.embeds:
+            if msg.embeds[0].video:
+                image_url = msg.embeds[0].video.url
+            else:
+                image_url = msg.embeds[0].url
+        else:
+            image_url = None
+        
+        if image_url:
+            print("IMAGE URL IS", image_url)
+            ms = MediaScaler()
+            media_file, filename = (await ms.download(image_url))
+            rfname, ext = await ms.rewrite_media(value, filename)
+            # if ext == 'mp4':
+            #     embed = discord.Embed(video=)
+            await ctx.send(file=discord.File(rfname))
+            await ms.remove_all_local()
+        print(msg.attachments)
+
+    @alanify.error
+    async def alanify_error(self, ctx, error):
+        print(error)
+        if isinstance(error, commands.MissingRequiredArgument) or isinstance(error, commands.BadArgument):
+            response = embed_template.copy()
+            response.description = "Invalid argument passed to 'alanify' command. Example: ?alanify 10"
     #msg.embeds.url has the image url
     #ctx.message.reference
     #?alan beautify 100 (fuckery factor)
--- modules/modification.py
+++ modules/modification.py
@@ -1,38 +1,110 @@
 from PIL import Image
 import asyncio
+import filetype
+import imageio
+# from moviepy.editor import *
 import tempfile
 import requests
 import time
 import os   
 import filetype
+import numpy
 
+ext_dict = {
+    "png" : "PNG-PIL",
+    "jpg" : "JPEG-PIL",
+    "gif" : "GIF-PIL",
+    "mp4" : "FFMPEG"
+} #ext_dict.keys() represents supported extensions
 
-class ImageScaler():
-    def __init__(self, url):
-        self.url = url
-
-    def scale_image(self):
-        (fd, filename) = tempfile.mkstemp()
+class MediaScaler():
+    """Set of classes used to rescale images, gifs, and videos (for ?alanify command)"""
+    def __init__(self):
+        self.local_files = [] #list of local files to be deleted later
+    
+    async def download(self, url):
+        fd, filename = tempfile.mkstemp()
         try:
             tfile = os.fdopen(fd, "wb")
             #fool discord into authorizing our request
-            r = requests.get(self.url, headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36'})
+            r = requests.get(url, headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36'})
+            #write to tempfile
             tfile.write(r.content)
             tfile.close()
-            print(filename)
-            #look at image type with pillow
-            print(filetype.guess(filename))
-            img = Image.open(filename)
-            print(img.format)
-            img.close()
+            #save as local_file
+            self.local_files.append(filename)
+            return (tfile, filename)
         except Exception as e:
             print("Could not download image")
             print(e)
-        finally:
-            os.remove(filename)
-        
+
+    async def remove_all_local(self):
+        for lf in self.local_files:
+            try:
+                os.remove(lf)
+            except Exception as e:
+                print("Could not delete local files!")
+                print(e)
+
+    async def rescale(self, factor, img : Image):
+        w, h = img.size
+        w_, h_ = w // factor, h // factor
+        img_ = img.resize((w_, h_))
+        img_out = img_.resize((w, h), Image.NEAREST)
+        return img_out
+
+    async def get_extension(self, filename):
+        kind = filetype.guess(filename)
+        print("HIHIIIII")
+        print(filename)
+        print(kind)
+        print(kind)
+        if kind is None or kind.extension not in ext_dict.keys():
+            print("Filetype not supported! Ping luke if you think this is a mistake")
+            print(kind)
+        else:
+            return kind.extension
+
+    async def rewrite_media(self, scale_factor, filename):
+        ext = await self.get_extension(filename)
+        if ext is None: return
+        reader = imageio.get_reader(filename, format=ext_dict[ext])
+        out_filename = f"{filename}_out.{ext}"
+        self.local_files.append(out_filename)
+        #format imageio writer properly
+        print(reader.get_meta_data())
+        if ext == 'mp4':
+            fps = reader.get_meta_data()['fps']
+            writer = imageio.get_writer(out_filename, fps=fps)
+        # elif ext == 'gif':
+        #     duration = reader.get_meta_data()['duration']
+        #     writer = imageio.get_writer(out_filename, mode='I', duration=5)
+        #     print('pass')
+        else:
+            writer = imageio.get_writer(out_filename)
+        #sequence each frame
+        for im in reader:
+            new_im = Image.fromarray(im)
+            fmt_img = await self.rescale(scale_factor, new_im)
+            writer.append_data(numpy.array(fmt_img))
+        writer.close()
+        #if file is mp4, re-add audio
+        # if ext == 'mp4':
+        #     audio_clip = AudioFileClip(filename)
+        #     video_clip = VideoFileClip(out_filename)
+        #     video_clip2 = video_clip.set_audio(audio_clip)
+        #     video_clip2.write_videofile(out_filename)
+        return (out_filename, ext)
+#alanify:
+#download file locally
+#get filetype
 
 
+# ms = MediaScaler()
+# media_file, filename = ms.download("https://media.discordapp.net/attachments/775075293345742879/829045040126492722/album_1f0afvdo6.gif")
+# print("downloaded", media_file, filename)
 
-img = ImageScaler("https://cdn.discordapp.com/attachments/780833671509966848/827245139591823400/video0.mp4")
-img.scale_image()
+# rfname = ms.rewrite_media(10, filename)
+# print(rfname)
+# ms.remove_all_local()
+
